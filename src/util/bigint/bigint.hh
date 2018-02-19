5ad21aa0fe65c6e4127d157c02422bf783490dee
math4tots <math4tots@gmail.com>
2018-02-19 14:51:29 -0800

    util/bigint

---
+++ src/util/bigint/bigint.hh
@@ -0,0 +1,217 @@
+#ifndef bigint_hh
+#define bigint_hh
+
+// Requires C++11
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct BigInt final {
+  using Digit = long;
+  using Sign = bool;
+  using Digits = std::vector<Digit>;
+
+  static constexpr const Digit BASE = 10000;
+  static constexpr const Digit DIGIT_WIDTH = 4;
+  static constexpr const Sign PLUS = true;
+  static constexpr const Sign MINUS = false;
+
+  Sign sign;
+  Digits digits;
+
+  BigInt(): sign(PLUS) {}
+  BigInt(Digit i) {
+    sign = i >= 0 ? PLUS : MINUS;
+    if (i < 0) {
+      i = -i;
+    }
+    while (i) {
+      digits.push_back(i % BASE);
+      i /= BASE;
+    }
+    remove_extraneous_zeros();
+  }
+  explicit BigInt(Sign s, Digits dd): sign(s), digits(std::move(dd)) {
+    remove_extraneous_zeros();
+    if (digits.empty()) {
+      sign = PLUS;
+    }
+  }
+
+  bool operator==(const BigInt &other) const {
+    return sign == other.sign && digits == other.digits;
+  }
+
+  bool operator<(const BigInt &other) const {
+    if (sign != other.sign) {
+      return sign < other.sign;
+    }
+    if (sign == MINUS) {
+      return other.digits < digits;
+    }
+    return digits < other.digits;
+  }
+
+  explicit operator bool() const { return !digits.empty(); }
+
+  BigInt pow(size_t n) const {
+    if (n == 0) {
+      return 1;
+    } else if (n == 1) {
+      return *this;
+    } else if (n % 2 == 0) {
+      auto i = pow(n / 2);
+      return i * i;
+    }
+    auto i = pow(n / 2);
+    return i * i * *this;
+  }
+
+  BigInt operator+(const BigInt &b) const {
+    if (sign == b.sign) {
+      return BigInt(sign, add(digits, b.digits));
+    }
+    if (sign == PLUS) {
+      return subtract(digits, b.digits);
+    }
+    return subtract(b.digits, digits);
+  }
+
+  BigInt operator*(const BigInt &b) const {
+    // TODO: Use better algorithm than long multiplication
+    BigInt ret;
+    for (size_t i = 0; i < digits.size(); i++) {
+      for (size_t j = 0; j < b.digits.size(); j++) {
+        ret = ret + BigInt(digits[i] * b.digits[j]).shift(i + j);
+      }
+    }
+    return ret;
+  }
+
+  BigInt operator-(const BigInt &b) const {
+    return *this + -b;
+  }
+
+  BigInt operator-() const {
+    return BigInt(!sign, digits);
+  }
+
+  std::string str() const {
+    std::stringstream ss;
+    write(ss);
+    return ss.str();
+  }
+
+  void write(std::ostream &out) const {
+    if (digits.empty()) {
+      out << "0";
+      return;
+    }
+    if (sign == MINUS) {
+      out << "-";
+    }
+    for (auto iter = digits.rbegin(); iter != digits.rend(); ++iter) {
+      std::stringstream ss;
+      ss << *iter;
+      auto digitstr = ss.str();
+
+      if (iter != digits.rbegin()) {
+        for (size_t j = 0; j < DIGIT_WIDTH - digitstr.size(); j++) {
+          out << "0";
+        }
+      }
+
+      out << digitstr;
+    }
+  }
+
+  size_t number_of_digits() const {
+    return digits.size();
+  }
+
+ private:
+
+  BigInt shift(size_t n) const {
+    Digits dd;
+    for (size_t i = 0; i < n; i++) {
+      dd.push_back(0);
+    }
+    for (auto digit: digits) {
+      dd.push_back(digit);
+    }
+    return BigInt(sign, std::move(dd));
+  }
+
+  void remove_extraneous_zeros() {
+    while (!digits.empty() && digits.back() == 0) {
+      digits.pop_back();
+    }
+  }
+
+  void flip_sign() {
+    sign = !sign;
+  }
+
+  static Digits add(const Digits &a, const Digits &b) {
+    Digit carry = 0;
+    Digits ret;
+    size_t i = 0;
+
+    while (i < a.size() || i < b.size() || carry) {
+      Digit aval = i < a.size() ? a[i] : 0;
+      Digit bval = i < b.size() ? b[i] : 0;
+      Digit next = aval + bval + carry;
+      ret.push_back(next % BASE);
+      carry = next / BASE;
+      i++;
+    }
+
+    while (!ret.empty() && ret.back() == 0) {
+      ret.pop_back();
+    }
+
+    return ret;
+  }
+
+  static std::pair<Digit, Digits>
+  subtract_helper(const Digits &a, const Digits &b) {
+    Digit borrow = 0;
+    Digits dd;
+    Sign sign = PLUS;
+    size_t i = 0;
+
+    while (i < a.size() || i < b.size()) {
+      Digit aval = i < a.size() ? a[i] : 0;
+      Digit bval = i < b.size() ? b[i] : 0;
+      Digit next = aval - bval - borrow;
+      borrow = 0;
+      while (next < 0) {
+        next += BASE;
+        borrow++;
+      }
+      dd.push_back(next);
+      i++;
+    }
+
+    return std::make_pair(sign, std::move(dd));
+  }
+
+  static BigInt subtract(const Digits &a, const Digits &b) {
+    auto pair1 = subtract_helper(a, b);
+    if (pair1.first == 0) {
+      return BigInt(PLUS, std::move(pair1.second));
+    }
+    auto pair2 = subtract_helper(b, a);
+    return BigInt(MINUS, std::move(pair2.second));
+  }
+};
+
+
+std::ostream &operator<<(std::ostream &out, const BigInt &b) {
+  b.write(out);
+  return out;
+}
+
+#endif//bigint_hh
---
+++ src/util/bigint/test.cc
@@ -0,0 +1,14 @@
+// g++ -std=c++11 test.cc && ./a.out
+#include "bigint.hh"
+#include <assert.h>
+
+int main() {
+  assert(BigInt(100).str() == "100");
+  assert(BigInt(1000000).str() == "1000000");
+  assert(BigInt(1000000).number_of_digits() == 2);
+  assert(BigInt(1000000) + BigInt(1000008) == BigInt(1000000 + 1000008));
+  assert(BigInt(17) * BigInt(724) == BigInt(17 * 724));
+  assert(BigInt(1000000).pow(2).str() == "1000000000000");
+  assert(BigInt(71484) - BigInt(901400) == BigInt(71484 - 901400));
+  assert((BigInt(0) - BigInt(4)).str() == "-4");
+}
